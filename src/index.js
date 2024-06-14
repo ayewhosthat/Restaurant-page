cda5d10f1032c8e16368816f6ebd48357350522a
ayewhosthat <alan.notiverson@gmail.com>
2024-06-14 17:02:48 -0400

    Add contact information to screen (tabbed browsing) when contact button is pressed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './style.css';
-import Birria from './birria.jpg'
+import Birria from './birria.jpg';
+import Phone from './phone.svg';
+import Email from './email-edit-outline.svg';
+const element = document.getElementById('content');
 
+// function to load the page
 function loadPage() {
-    const element = document.getElementById('content');
     const myIcon = new Image();
     const title = document.createElement('h1');
     title.textContent = 'Birria? Birria Tacoz';
@@ -18,3 +21,39 @@ function loadPage() {
 }
 
 loadPage();
+
+const home = document.querySelector('.home');
+home.addEventListener('click', loadPage);
+
+function clearPage() {
+    element.innerHTML = '';
+}
+
+const contact = document.querySelector('.contact');
+contact.addEventListener('click', () => {
+    clearPage();
+    const contactDiv = document.createElement('div');
+    contactDiv.classList.add('contact-div');
+    const text = document.createElement('h1');
+    text.textContent = 'Contact us!';
+    contactDiv.appendChild(text);
+    const phoneDiv = document.createElement('div');
+    phoneDiv.classList.add('contact-method');
+    const phone = document.createElement('h2');
+    phone.textContent = 'Phone: (123)-456-TACO';
+    const phoneIcon = new Image();
+    phoneIcon.src = Phone;
+    phoneDiv.appendChild(phoneIcon);
+    phoneDiv.appendChild(phone);
+    contactDiv.appendChild(phoneDiv);
+    const emailDiv = document.createElement('div');
+    const email = document.createElement('h2');
+    email.textContent = 'management@birriabirriatacoz.com';
+    const emailIcon = new Image();
+    emailIcon.src = Email;
+    emailDiv.appendChild(emailIcon);
+    emailDiv.appendChild(email)
+    contactDiv.appendChild(emailDiv);
+
+    element.appendChild(contactDiv);
+});
