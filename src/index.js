5451f67ec3c209125ef15466d221a9aa4cfe6bea
ayewhosthat <alan.notiverson@gmail.com>
2024-06-14 16:08:11 -0400

    Add h2 + page styling

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ function loadPage() {
     element.appendChild(title);
     myIcon.src = Birria;
     element.appendChild(myIcon); // append image to content container
-    const description = document.createElement('h4');
+    const attention = document.createElement('h2');
+    attention.textContent = 'Why choose us?';
+    element.appendChild(attention);
+    const description = document.createElement('h3');
     description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     element.appendChild(description);
 }
